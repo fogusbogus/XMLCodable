03df8b8e15b58cda241ef12f40fde4863fa81085
Matt Hogg <matt.hogg74@hotmail.com>
2026-06-07 21:05:47 +0100

    Create an XMLNodeTree from some xml

--- Sources/XMLCodable/XMLCodable.swift
+++ Sources/XMLCodable/XMLCodable.swift
@@ -1,2 +1,116 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public class XMLNode: Codable {
+	public init(_ name: String, namespace: String? = nil, qName: String? = nil, attributes: [String : String] = [:], children: [XMLNode] = []) {
+		self.children = children
+		self.name = name
+		self.namespace = namespace
+		self.qName = qName
+		self.attributes = attributes
+		self.path = "/"
+	}
+	
+	public func find(_ key: String) -> [XMLNode] {
+		if keyMatch(key) { return [self] }
+		var ret: [XMLNode] = []
+		children.forEach { child in
+			ret.append(contentsOf: child.find(key))
+		}
+		return ret
+	}
+	
+	private func keyMatch(_ key: String) -> Bool {
+		if key.contains(":") {
+			let items = key.split(separator: ":").map {String($0).trimmingCharacters(in: .whitespacesAndNewlines)}
+			if let namespace, namespace.localizedCaseInsensitiveCompare(items.first!) == .orderedSame {
+				if name.localizedCaseInsensitiveCompare(items[1]) == .orderedSame {
+					return true
+				}
+			} else {
+				return name.localizedCaseInsensitiveCompare(items[1]) == .orderedSame
+			}
+		}
+		return name.localizedCaseInsensitiveCompare(key) == .orderedSame
+	}
+	
+	public subscript(address: String) -> [XMLNode] {
+		get {
+			let split = address.split(separator: "/", omittingEmptySubsequences: false).map {String($0)}
+			return self[split]
+		}
+	}
+	
+	public subscript(address: [String]) -> [XMLNode] {
+		get {
+			guard address.count > 0 else { return [] }
+			var ret: [XMLNode] = []
+			var node: XMLNode? = self
+			var address = address
+
+			if address.first!.isEmpty {
+				address.removeFirst()
+			}
+			while address.count > 0 && node != nil {
+				if address.count == 1 {
+					ret.append(contentsOf: node!.children.filter {$0.keyMatch(address.first!)})
+					address = []
+				} else {
+					node = node?.children.first(where: {$0.keyMatch(address.first!)})
+					address.removeFirst()
+				}
+			}
+
+			return ret
+		}
+	}
+	
+	public var children: [XMLNode] = []
+	public var name: String
+	public var namespace: String?
+	public var qName: String?
+	public var attributes: [String:String]
+	public var text: String?
+	public private(set) var path: String
+	
+	public func attr<T>(_ key: String, _ defaultValue: T) -> T {
+		guard attributes.keys.contains(key) else { return defaultValue }
+		return (attributes[key] as? T) ?? defaultValue
+	}
+	
+	internal func setPath(_ pathName: String) {
+		if pathName.hasSuffix("/") {
+			path = pathName + getKey()
+		} else {
+			path = "\(pathName)/\(getKey())"
+		}
+		children.forEach {$0.setPath(path)}
+	}
+		
+	private func getKey() -> String {
+		if let namespace {
+			return "\(namespace):\(name)"
+		}
+		return name
+	}
+}
+
+
+public class XMLNodeTree: Codable {
+	public var topNode: XMLNode? {
+		didSet {
+			topNode?.setPath("/")
+		}
+	}
+	
+	public subscript(key: String) -> [XMLNode] {
+		get {
+			return topNode?[key] ?? []
+		}
+	}
+	
+	public init(topNode: XMLNode? = nil) {
+		self.topNode = topNode
+		topNode?.setPath("/")
+	}
+}
+
--- Tests/XMLCodableTests/XMLCodableTests.swift
+++ Tests/XMLCodableTests/XMLCodableTests.swift
@@ -1,8 +1,22 @@
 import Testing
+import Foundation
 @testable import XMLCodable
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     // Swift Testing Documentation
     // https://developer.apple.com/documentation/testing
+	let xml = "<?xml version=\"1.0\"?><someTag someAttr anotherAttr=\"Some data\" />"
+	let url = URL(filePath: "/Users/matt/Movies/RHPS Subtitles ITT Format_iTT_English (United States).itt")!
+	let parser = ITunesTimedTextParser(contentsOf: url)! //MemoParser(data: xml.data(using: .utf8)!)
+	parser.parse()
+	if let error = parser.parserError {
+		print(error)
+	}
+	let jsonS = JSONEncoder()
+	print(try String(data: jsonS.encode(parser.nodes.first!), encoding: .utf8)!)
+	try String(data: jsonS.encode(parser.nodes.first!), encoding: .utf8)?.write(toFile: "/Users/matt/ittSample.json", atomically: true, encoding: .utf8)
 }
+
+
+
